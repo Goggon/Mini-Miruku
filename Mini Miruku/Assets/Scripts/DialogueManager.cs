786f16d3e692c436e77ed78288dc093884cd78bb
Goggon <bettemoller@hotmail.com>
2018-11-03 02:32:04 +0100

    Dialogue system beginnings

---
+++ Mini Miruku/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public Text NameText;
+    public Text DialogueText;
+
+    private Queue<string> lines;
+
+	// Use this for initialization
+	void Start () {
+        lines = new Queue<string>();
+	}
+	
+    public void StartDialogue (Dialogue dialogue)
+    {
+        NameText.text = dialogue.name;
+
+        lines.Clear();
+
+        foreach (string line in dialogue.lines)
+        {
+            lines.Enqueue(line);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (lines.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string line = lines.Dequeue();
+
+        DialogueText.text = line;
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("end of conversation");
+    }
+}
---
+++ Mini Miruku/Assets/Scripts/TriggerEnter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnter : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        
+    }
+}
