dadd10514c6428def8057da30824e99a885a7406
Goggon <bettemoller@hotmail.com>
2018-11-15 15:54:58 +0100

    shit for the day

--- Mini Miruku/Assets/Scripts/DialogueManager.cs
+++ Mini Miruku/Assets/Scripts/DialogueManager.cs
@@ -9,25 +9,33 @@ public class DialogueManager : MonoBehaviour {
     public Text DialogueText;
 
     public Animator animator;
+    public Animator Char1;
 
     private Queue<string> lines;
+    private Queue<string> Names;
 
 	// Use this for initialization
 	void Start () {
         lines = new Queue<string>();
+        Names = new Queue<string>();
 	}
 	
     public void StartDialogue (Dialogue dialogue)
     {
         animator.SetBool("IsOpen", true);
 
-        NameText.text = dialogue.name;
+        //NameText.text = dialogue.name;
 
         lines.Clear();
+        Names.Clear();
 
         foreach (string line in dialogue.lines)
         {
             lines.Enqueue(line);
+        }
+        foreach (string name in dialogue.names)
+        {
+            Names.Enqueue(name);
         }
 
         DisplayNextSentence();
@@ -42,13 +50,25 @@ public class DialogueManager : MonoBehaviour {
         }
 
         string line = lines.Dequeue();
+        string name = Names.Dequeue();
 
         DialogueText.text = line;
+        NameText.text = name;
+
+        if (name == "Jeff")
+        {
+            Char1.SetBool("Char1", true);
+        }
+        else if (name == "Bob")
+        {
+            Char1.SetBool("Char1", false);
+        }
     }
 
     void EndDialogue()
     {
         animator.SetBool("IsOpen", false);
+        Char1.SetBool("Char1", false);
 
         Debug.Log("end of conversation");
     }
