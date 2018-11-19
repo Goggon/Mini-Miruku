f1fca074ac0210ce476de9df1d5beb4c3e2178bc
Goggon <bettemoller@hotmail.com>
2018-11-19 21:41:02 +0100

    enemy Patrol virker og combat er næsten good 2 go

---
+++ Mini Miruku/Assets/Scripts/Player Scripts/CombatPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatPlayer : MonoBehaviour {
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemy;
+    public float attackRange;
+    public int damage;
+
+
+	// Update is called once per frame
+	void Update () {
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Collider2D[] enemiesToDmg = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy);
+                for (int i = 0; i < enemiesToDmg.Length; i++)
+                {
+                   enemiesToDmg[i].GetComponent<Enemy>().takedamage(damage);
+                }
+
+                timeBtwAttack = startTimeBtwAttack;
+            }
+
+            timeBtwAttack = startTimeBtwAttack;
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+	}
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
---
+++ Mini Miruku/Assets/Scripts/enemy/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed;
+    private float waittime;
+    public float startwaittime;
+
+    public Transform[] movespots;
+    private int randomspot;
+
+    public int health;
+    private Animator anim;
+
+
+    private void Start()
+    {
+        waittime = startwaittime;
+        randomspot = Random.Range(0, movespots.Length);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, movespots[randomspot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, movespots[randomspot].position) < 0.2F)
+        {
+            if(waittime <= 0)
+            {
+                randomspot = Random.Range(0, movespots.Length);
+                waittime = startwaittime;
+            }
+            else
+            {
+                waittime -= Time.deltaTime;
+            }
+        }
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void takedamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("took dmg");
+    }
+}
